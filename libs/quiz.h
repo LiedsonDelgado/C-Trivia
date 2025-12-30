26633f107104d140cb48dba186b39446cf70b286
Tiago <tiagodongo521@gmail.com>
2025-12-30 21:28:24 -0100

    feat: add data strctures
    - add user structure
    - add question structure

--- libs/quiz.h
+++ libs/quiz.h
@@ -1,6 +1,6 @@
 /*
 -> File Description
--> 
+-> Ficheiro responsavel por  armazenar headers e as assinaturas de funções
 */
 
 #ifndef QUIZ_H_INCLUDED
@@ -10,6 +10,20 @@
 #include<stdlib.h>
 #include<string.h>
 
+//arvore de usuarios
+typedef struct user{
+    char playerName[100];
+    int score;
+    struct users *left;
+    struct users *right;
+}User;
 
+//lista de perguntas
+typedef struct question{
+    char *question;
+    char *options;
+    char correctAnswer;
+    struct question *next;
+}Question;
 
 #endif
