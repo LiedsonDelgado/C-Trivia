613dffffff25b3ee6b031baaeb7fdaf8b126945a
Liedson Delgado <liedsondelgado38@gmail.com>
2025-12-30 23:30:00 -0100

    Update quiz.c

--- src/quiz.c
+++ src/quiz.c
@@ -1,1 +1,32 @@
 #include"quiz.h"
+
+void menu(){
+    printf("1 - Jogar\n");
+    printf("2 - Adicionar nova pergunta\n");
+    printf("3 - Eliminar pergunta\n");
+    printf("4 - Mostrar todas as perguntas");
+    printf("5 - Mostrar classificação\n");
+    printf("6 - Procurar jogador\n");
+    printf("0 - Sair\n");
+}
+
+QuestionList addNewQuestion(QuestionList qList,Question question){
+
+    return qList;
+}
+
+
+void eliminateQuestion(QuestionList qList,Question question){
+
+}
+void showAllTheQuestions(QuestionList qList){
+
+}
+
+void showScore(Question qList){
+
+}
+char* searchPlayer(Player playerTree){
+
+    return->playerTree->playerName;
+}
