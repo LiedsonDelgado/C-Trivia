3275869a56c54c26f86135983f230a09b3d6c4eb
Liedson Delgado <liedsondelgado38@gmail.com>
2026-01-10 20:41:56 -0100

    Add files via upload

--- libs/quiz.h
+++ libs/quiz.h
@@ -1,6 +1,6 @@
 /*
 -> File Description
--> Ficheiro responsavel por  armazenar headers e as assinaturas de funções
+-> Ficheiro responsavel por armazenar headers e as assinaturas de funções
 */
 
 #ifndef QUIZ_H_INCLUDED
@@ -9,23 +9,43 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<locale.h>
 
-//arvore de usuarios
-typedef struct user{
+//Estruturas de Dados
+//-------------------
+typedef char* Question;
+
+//Estrutura que representa a arvore de usuarios/jogadores
+typedef struct player{
     char playerName[100];
     int score;
-    struct users *left;
-    struct users *right;
-}User;
+    struct player *left;
+    struct player *right;
+}Player;
 
-//lista de perguntas
-typedef struct question{
-    char *question;
-    char *options;
-    char correctAnswer;
-    struct question *next;
-}Question;
+//Estrutura da lista de perguntas
+typedef struct questionList{
+    Question question; //guarda a struct quiz que contem a pegunta e o ranking do jogador
+    char options; //contendo apenas um caracter(ex.:A,B,C,D,etc)
+    Question correctAnswer; //resposta correta da pergunta
+    struct questionList *next;
+}QuestionList;
 
+//Prototipos/Assinatura das Funcoes
+//----------------------
 void menu();
+
+Player* createPlayerTreeNode(); //cria a raiz da arvore e inicializa a arvore
+void addPlayerIntoTree(Player** tree,char playerName[100],int score); //adiciona o no que contem informacoes do jogador na arvore
+void removePlayerFromTree(Player** tree,char playerName[100]); //remove o no que contem informacoes do jogador na arvore
+
+QuestionList* createQuestionList(); //cria o primeiro no da lista de questoes
+QuestionList* addNewQuestion(QuestionList* qList,QuestionList* question,Question* correctAnswer); //recebe como parametro a lista de questoes e a questao que se quer adicionar nela
+void eliminateQuestion(QuestionList* qList,QuestionList* question,Question* correctAnswe); //seleciona o elemento na lista guardar ele numa var auxiliar e ajustar a lista e apagar ele
+
+char* searchPlayer(Player* playerTree);
+
+void showAllTheQuestions(QuestionList* qList);
+void showScore(QuestionList* qList);
 
 #endif
--- libs/util.h
+++ libs/util.h
@@ -1,15 +1,21 @@
-/*
--> File Description
--> 
-*/
+//funções auxiliares
 
 #ifndef UTIL_H_INCLUDED
 #define UTIL_H_INCLUDED
 
-#include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 
-int validateInt(const char *prompt);
+int validateInt(const char *prompt); //valida as entradas do usuario(seja jogador ou administrador de sistema)
+char generateCorrectOption();
+
+/*
+- valida as entradas de strings  do usuario
+- parametros:
+    - prompt: label do input
+    - destination: local onde a  string sera guardada
+    - maxLenght: tamanho maximo da string
+*/
 int validateString(const char *prompt, char *destination, int maxLenght);
 
 #endif
